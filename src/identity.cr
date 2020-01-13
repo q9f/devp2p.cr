7cc6a4d343cb608d85c8a79d81b9844a070bcad3
/raw PONG _GHMoaCXLT <58883403+q9f@users.noreply.github.com>
2020-01-13 15:15:40 +0100

    add a devp2p client structure

--- src/identity.cr
+++ src/identity.cr
@@ -14,23 +14,100 @@
 
 require "secp256k1"
 
-module Devp2p
-  class Identity
+# Implements the `Devp2p::Identity` module handling any kind of p2p node addresses
+# and client identifiers.
+module Devp2p::Identity
+  # A `Client` is a node implementing the `Devp2p` module and usually comes with
+  # name, identity, and a version.
+  #
+  # Properties:
+  # * `name` (`String`): the name of the `Client` which should be consistant across an implementation.
+  # * `identity` (`(String)?`): a customizable identity.
+  # * `version` (`String`): a sensible version string, e.g., `1.9.7`.
+  #
+  # ```
+  # cli = Devp2p::Identity::Client.new "Cryeth", "Superhero42", "1.9.7"
+  # cli.to_s
+  # # => "Cryeth/Superhero42/v1.9.7"
+  # ```
+  class Client
+    # The name of the `Client` which should be consistant across an implementation.
+    property name : String
+
+    # A customizable identity.
+    property identity : (String)?
+
+    # A sensible version string, e.g., `1.9.7`.
+    property version : String
+
+    # Generates a default `devp2p-cr-node` client.
+    #
+    # ```
+    # cli = Devp2p::Identity::Client.new
+    # cli.to_s
+    # # => "devp2p-cr-node/v0.1.0-crystalv0.32.1"
+    # ```
+    def initialize
+      @name = "devp2p-cr-node"
+      @version = "v#{VERSION}-crystalv#{Crystal::VERSION}"
+    end
+
+    # Generates a custamizable client coming with name, identity, and a version.
+    #
+    # Parameters:
+    # * `name` (`String`): The name of the `Client` which should be consistant across an implementation.
+    # * `identity` (`(String)?`): A customizable identity.
+    # * `version` (`String`): A sensible version string, e.g., `1.9.7`.
+    #
+    # ```
+    # cli = Devp2p::Identity::Client.new "Cryeth", "Superhero42", "1.9.7"
+    # cli.to_s
+    # # => "Cryeth/Superhero42/v1.9.7"
+    # ```
+    def initialize(@name, @identity, @version)
+    end
+
+    # Returns a string literal containing client name, identity, and version.
+    #
+    # ```
+    # cli.to_s
+    # # => "devp2p-cr-node/v0.1.0-crystalv0.32.1"
+    # ```
+    def to_s
+      id = "#{@name}/#{@identity}/#{@version}"
+      id = "#{@name}/#{@version}" if @identity.nil?
+      return id
+    end
+  end
+
+  # A `Node` is a peer in a `Devp2p` network containing an `Secp256k1::Ethereum::Enode` address and a `Client` identifier.
+  #
+  # Properties:
+  # * `enode` (`Secp256k1::Ethereum::Enode`): A `Devp2p` enode containing a `Secp256k1::Keypair` and an IP address.
+  #
+  # ```
+  # nod = Devp2p::Identity::Node.new
+  # nod.to_s
+  # # => "enode://bf0cf8c934bd3c57e962fdf2a47e99d6136b047f987ee2e0cb03110cafd92afc981974428f8162d3f8fce2f58d4e56341478e87d092aeb3a0edf8af97d638d04@31.33.3.37:31337"
+  # ```
+  class Node
+    property client : Devp2p::Identity::Client
     property enode : Secp256k1::Ethereum::Enode
 
     def initialize
+      @client = Devp2p::Identity::Client.new
       @enode = Secp256k1::Ethereum::Enode.new
     end
 
-    def initialize(keys)
+    def initialize(@client, keys : Secp256k1::Keypair)
       @enode = Secp256k1::Ethereum::Enode.new keys
     end
 
-    def initialize(keys, port)
+    def initialize(@client, keys : Secp256k1::Keypair, port : Int32)
       @enode = Secp256k1::Ethereum::Enode.new keys, port
     end
 
-    def initialize(keys, ip, port)
+    def initialize(@client, keys : Secp256k1::Keypair, ip : String, port : Int32)
       @enode = Secp256k1::Ethereum::Enode.new keys, ip, port
     end
 
@@ -40,6 +117,10 @@ module Devp2p
 
     def get_ip_and_port
       return @enode.address
+    end
+
+    def get_client_version
+      return @client.to_s
     end
 
     def to_s
